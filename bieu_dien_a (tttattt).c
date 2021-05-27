691e2ceaa687e0021d8ea46a02fafbc95e8b9769
dung1202 <70959545+dung1202@users.noreply.github.com>
2021-05-27 13:28:24 +0700

    Update bieu_dien_a (tttattt).c

--- bieu_dien_a (tttattt).c
+++ bieu_dien_a (tttattt).c
@@ -15,15 +15,16 @@ void nhap()
 
 void bieu_dien_a(p, w, a)
 {
-    int m, t, uoc, bieudien, j = 0;
+    int m, t, uoc;
+    int j = 0;
     m = (log(p) / log(2)) + 1;
     t = (m / w) + 1;
-    bieudien = a;
+    // printf("m = %d, t = %d", m, t);
     printf("cac he so la");
     for (int i = t - 1; i >= 0; i--)
     {
-        uoc = bieudien / pow(2, i * w);
-        bieudien = bieudien - (pow(2, i * w) * uoc);
+        uoc = a / pow(2, i * w);
+        a = a - (pow(2, i * w) * uoc);
         printf("\nhe so thu %d = %d", j + 1, uoc);
         j++;
     }
@@ -33,6 +34,5 @@ int main()
 {
     nhap();
     bieu_dien_a(p, w, a);
-    getch();
     return 0;
 }
